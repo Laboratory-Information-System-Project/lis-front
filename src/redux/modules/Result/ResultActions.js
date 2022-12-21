51404fddf74819bcc208d42d98b144029ddaa9bd
Hyynminini <gusals960119@gmail.com>
2022-12-21 15:37:39 +0900

    final ResultPage Update

--- src/redux/modules/Result/ResultActions.js
+++ src/redux/modules/Result/ResultActions.js
@@ -1,6 +1,7 @@
 import Types from '../../ActionConstants';
 import * as ResultApi from '../../../api/ResultApi';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const ResultActions = {
     getDateSearch:
@@ -32,6 +33,20 @@ const ResultActions = {
                     type: Types.GET_RESULTS_FAILURE,
                     payload: error.toString(),
                 });
+                if (error.response.status === 401) {
+                    Swal.fire({
+                        title: '계정이 만료되었습니다.',
+                        icon: 'error',
+                        confirmButtonColor: '#3C9DF6',
+                        confirmButtonText: '확인',
+                    }).then((result) => {
+                        window.localStorage.removeItem('AccessToken');
+                        window.localStorage.removeItem('authority');
+                        window.localStorage.removeItem('username');
+                        window.localStorage.removeItem('userId');
+                        window.location.href = '/';
+                    });
+                }
             }
         },
 
@@ -54,6 +69,20 @@ const ResultActions = {
                 type: Types.GET_SEARCH_RESULTS_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
 
@@ -75,6 +104,20 @@ const ResultActions = {
                 type: Types.POST_RESULT_SMS_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
     getSmsData: () => async (dispatch) => {
@@ -96,6 +139,20 @@ const ResultActions = {
                 type: Types.GET_SMSDATA_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
     postSmsData: (smsTitle, smsContent) => async (dispatch) => {
@@ -116,6 +173,20 @@ const ResultActions = {
                 type: Types.POST_SMSDATA_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
     deleteSmsData: (smsNo) => async (dispatch) => {
@@ -137,6 +208,20 @@ const ResultActions = {
                 type: Types.DELETE_SMSDATA_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
     editSmsData: (smsNo, smsTitle, smsContent) => async (dispatch) => {
@@ -161,6 +246,20 @@ const ResultActions = {
                 type: Types.PUT_SMSDATA_FAILURE,
                 payload: error.toString(),
             });
+            if (error.response.status === 401) {
+                Swal.fire({
+                    title: '계정이 만료되었습니다.',
+                    icon: 'error',
+                    confirmButtonColor: '#3C9DF6',
+                    confirmButtonText: '확인',
+                }).then((result) => {
+                    window.localStorage.removeItem('AccessToken');
+                    window.localStorage.removeItem('authority');
+                    window.localStorage.removeItem('username');
+                    window.localStorage.removeItem('userId');
+                    window.location.href = '/';
+                });
+            }
         }
     },
 };
