bc04344be74d40f0dd79f43bc4bc784026890139
liverbird <ehdgns07@gmail.com>
2022-12-24 23:12:56 +0900

    mistake

--- src/components/Register/RegisterItem.js
+++ src/components/Register/RegisterItem.js
@@ -1,10 +1,21 @@
+
+import styled from "@emotion/styled";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 const RegisterItem = ({
-    barcode,dataInfo,userName,collectingDt,orderCode,vesselCode,sampleCode,prescribeCode,statusName,collectorId,data
-}) => {
+                          barcode,dataInfo,userName,collectingDt,orderCode,vesselCode,sampleCode,prescribeCode,statusName,collectorId,data
+                      }) => {
 
+    if(statusName === '채혈'){
+        var Color = styled('td')({
+            backgroundColor: '#5FE73A'
+        });
+    }else if(statusName === '검체접수'){
+        var Color = styled('td')({
+            backgroundColor: '#E9B065'
+        });
+    }
     const[a,setA]= useState('-');
     const[b,setB]= useState('-');
 
@@ -12,9 +23,9 @@ const RegisterItem = ({
         setA("-")
         setB("-")
         dataInfo?.data?.length > 0 && dataInfo.data.map((data2)=>{
-            if(data.prescribeCode === data2.prescribe_code){
-                if(data2.unsuitable_status_code === "CU"){
-                        setA("CU") 
+                if(data.prescribeCode === data2.prescribe_code){
+                    if(data2.unsuitable_status_code === "CU"){
+                        setA("CU")
                     }else if(data2.unsuitable_status_code === "SU"){
                         setB("SU")
                     }
@@ -23,12 +34,12 @@ const RegisterItem = ({
             }
         );
     },[dataInfo,data.prescribeCode])
-    
+
     return(
-        
+
         <tr>
             <td>{barcode}</td>
-            <td>{statusName}</td>
+            <Color>{statusName}</Color>
             <td>{collectingDt}</td>
             <td>{userName} / {collectorId}</td>
             <td>{orderCode}</td>
